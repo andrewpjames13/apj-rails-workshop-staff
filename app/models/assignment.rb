1fe13630486611ac55f4a146a0bdd0b5df632033
Andrew James <andrewpjames87@gmail.com>
2015-02-27 11:43:55 -0700

    User can edit assignment and not change anything but still saves and redirects

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -7,7 +7,7 @@ class Assignment < ActiveRecord::Base
   validates :location_id, :person_id, :roles, presence: true
 
   def unique_location
-    if person.assignments.find_by(location_id: location_id, roles: roles)
+    if person.assignments.where('id != ?', id).find_by(location_id: location_id, roles: roles)
       errors.add(:base, "People cannot be assigned to the same role in the same location")
     end
   end
