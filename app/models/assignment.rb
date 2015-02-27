d72926e9acf35dc6fb42da3654cf3e5762d07e2f
Andrew James <andrewpjames87@gmail.com>
2015-02-27 11:22:44 -0700

    User can have the same role in different locations

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,7 +3,13 @@ class Assignment < ActiveRecord::Base
   belongs_to :location
   belongs_to :person
 
-  validates :roles, :location_id, presence: true
-  validates :roles, uniqueness: true
+  validate :unique_location
+  validates :location_id, :person_id, :roles, presence: true
+
+  def unique_location
+    if person.assignments.find_by(location_id: location_id, roles: roles)
+      errors.add(:base, "People cannot be assigned to the same role in the same location")
+    end
+  end
 
 end
